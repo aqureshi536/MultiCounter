e247c343495c38b21eb4abf6f88bb64b9c9205d4
Ahmad Qureshi <aqureshi536@gmail.com>
2020-10-11 15:08:10 +0530

    initialcommit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,107 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // header: string = "Default"
+  isRenameCountEnabled: boolean = false;
+
+  @ViewChild(IonSlides) slides: IonSlides;
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    autoHeight : true,
+  }
+  slideIndex: number = 0;
+  headerInput : any;
+  isNew = true;
+
+  tabs = [{
+    name: "counter",
+    header: "Default"
+  }, {
+    name: "count_list",
+    header: "Saved"
+  }]
+
+  countBaseObject = {
+    name: "",
+    count: 0,
+    start: "",
+    end: ""
+
+  };
+
+  countData: any[] = [
+    
+  ];
+
+  count: number = this.getBaseCount();
+
+
+
+  constructor() { }
+
+  increment(value) {
+    if (value == undefined)
+      value = 1;
+    this.count = this.count + value;
+  }
+
+  reset() {
+    if (this.count > 0)
+      this.count = 0;
+  }
+
+  getBaseCount(): number {
+    return 0;
+  }
+
+  slideChanged() {
+    let index = this.slides.getActiveIndex()
+    index.then((value) => {
+      this.slideIndex = value;
+    });
+  }
+
+  renameCountName() {
+    this.isRenameCountEnabled = true;
+  }
+
+  closeRenameCountName() {
+    this.isRenameCountEnabled = false;
+  }
+
+  gotoCountList() {
+    this.slideIndex = 1;
+    this.slideTo(this.slideIndex);
+  }
+
+  gotoMainCount() {
+    this.slideIndex = 0;
+    this.slideTo(this.slideIndex)
+  }
+
+
+  slideTo(index) {
+    this.slides.slideTo(index, 400);
+  }
+
+
+  getTabName() {
+    return this.tabs[this.slideIndex].name;
+  }
+
+  saveCountData(){
+    if(this.isNew){
+      let object= Object.assign({},this.countBaseObject);
+      object.name = this.headerInput;
+      object.count = this.count;
+      this.countData.push(object);
+      
+    }
+  
+    this.tabs[0].header = this.headerInput;
+    this.headerInput = "";
+    this.isRenameCountEnabled = false;
+  }
 
 }
